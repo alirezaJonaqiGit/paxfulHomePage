613248692d616c662347a851ca129c12895107e3
alireza jonaqi <alirezajonaqinet@gmail.com>
2022-05-14 16:59:13 +0430

    headerCreated

---
+++ app.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('.cryptoItem').forEach((element) => {
+    element.addEventListener('click', function () {
+        let cryptoTag =  this.children[0].innerHTML;
+        document.querySelector('#dropdown_btnTag').innerHTML = cryptoTag;
+    });
+});
+
+
+// payment methods bages group
+// با کلیک روی هر کدام ، کلاس اکتیو بده بهش =)
+let badgeGroupElement = document.querySelector('.paymentMethodsBadgesGroup');
+[...badgeGroupElement.children].forEach((element) => {
+    element.addEventListener('click', (e) => {
+        // به عنصری که روش کلیک شد کلاس اکتیو بده و سایر کلاس های اکتیو رو غیر فعال کن
+        let activeBadge = document.querySelector('.paymentMethodsBadgesGroup>.badgeActive');
+        if (activeBadge !== undefined) {
+            activeBadge.classList.remove('badgeActive');
+        }
+        e.target.classList.add('badgeActive');
+        
+        // set the innerHTML of .paymentMethodsModalBtn == activated badge 
+        document.querySelector('.paymentMethodsModalBtn').innerHTML = e.target.innerHTML;
+    });
+});
+
+
+document.querySelector('.spendAmount_spendValueInput').addEventListener('keydown', function () {
+    
+})
+
